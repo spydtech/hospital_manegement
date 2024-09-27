0664d72df977098025c40be708829d7c597793d6
Ashwini Kumari <ashwini.swe@gmail.com>
2024-09-27 16:59:48 +0530

    add master page patient page or reportsMasters page and correct login page

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,9 @@ import Home from "./Components/home/Home";
 import Login from "./components/pages/Login/login";
 import SignUp from "./components/pages/signUp/signUp";
 import BookAppointment from "./components/BookAppointment"
+import ReportsMasters from "./components/admin/reports&Masters/ReportsMasters";
+import Master from "./components/admin/master/Master";
+import Patient from "./components/admin/patient/Patient";
 // import "./App.css";
 
 function App() {
@@ -13,6 +16,9 @@ function App() {
         <Route path="/login" element={<Login />} />
         <Route path="/sing_up" element={<SignUp />} />
         <Route path="/bookappointment" element={<BookAppointment />} />
+        <Route path="/reportsmasters" element={<ReportsMasters />} />
+        <Route path="/masters" element={<Master />} />
+        <Route path="/patient" element={<Patient />} />
       </Routes>
     </Router>
   );
---
+++ src/components/admin/master/Master.jsx
@@ -0,0 +1,40 @@
+// import React from 'react';
+import { Link } from 'react-router-dom';
+import { FiArrowRight } from 'react-icons/fi';
+import { MdArrowOutward } from "react-icons/md";
+const Master = () => {
+  // Define button data dynamically
+  const buttonData = [
+    { label: 'Patient', path: '/patient' },
+    { label: 'Healthcare Practitioner', path: '/healthcare-practitioner' },
+    { label: 'Practitioner Schedule', path: '/practitioner-schedule' },
+    { label: 'Medical Department', path: '/medical-department' },
+    // Add more buttons if needed
+  ];
+
+  return (
+    <div className="min-h-screen flex flex-col justify-start max-w-7xl items-center py-10">
+      {/* Heading */}
+      <h1 className="text-4xl lg:w-full lg:max-w-[940px]   mb-8">
+       <span className="font-bold  text-transparent bg-clip-text bg-gradient-to-l from-[#E53E13] to-[#17469E]"> Masters</span>
+      </h1>
+
+      {/* Button Grid */}
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
+        {buttonData.map((button, index) => (
+          <Link
+            key={index}
+            to={button.path}
+            className="bg-[#17469E] text-white py-4 px-8 lg:w-[458px] lg:h-[100px] flex justify-between items-center rounded-md shadow-lg "
+          >
+            <span>{button.label}</span>
+            <MdArrowOutward size={20} />
+          </Link>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Master;
+
