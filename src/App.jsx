599fc70a809c3736a1d81aff8bc86dc7ca72ae83
Ganesh Nitta <84185648+NITTAGANESH@users.noreply.github.com>
2024-09-28 19:45:23 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Home from "./Components/home/Home";
+import Home from "./components/home/Home";
 import Login from "./components/pages/login/Login";
 import SignUp from "./components/pages/signUp/signUp";
 import BookAppointment from "./components/BookAppointment"
@@ -10,16 +10,7 @@ import HealthcarePractitioner from "./components/admin/master/healthcarePractiti
 import NewPractitionerSchedule from "./components/admin/master/healthcarePractitioner/NewPractitionerSchedule"
 import EditPractitionerSchedule from "./components/admin/master/healthcarePractitioner/EditPractitionerSchedule"
 // import "./App.css";
-import Practitioner from "./components/admin/master/practitionerSchedule/PractitionerSchedule"
-import MedicalDapartment from "./components/admin/master/medicalDapartment/MedicalDapartment";
-import ConsultationSetup from "./components/admin/consultationSetup/ConsultationSetup";
-import Consultation from "./components/admin/consultation/Consultation";
-import FacilityManagement from "./components/admin/facility_Management/FacilityManagement";
-import PatientApointment from "./components/admin/consultation/patientAppointment/PatientApointment";
-import HealthCareServiceUnit from "./components/admin/facility_Management/healthCareServiceUnit/HealthCareServiceUnit";
-import AppointmentType from "./components/admin/consultationSetup/appointmentType/AppointmentType"
-import NewAppointmentType from "./components/admin/consultationSetup/appointmentType/NewAppointmentType"
-import EditAppointmentType from "./components/admin/consultationSetup/appointmentType/EditAppointmentType"
+import Practitioner from "./components/admin/master/practitionerSchedule/Practitioner"
 function App() {
   return (
     <BrowserRouter>
@@ -28,35 +19,13 @@ function App() {
         <Route path="/login" element={<Login />} />
         <Route path="/sing_up" element={<SignUp />} />
         <Route path="/bookappointment" element={<BookAppointment />} />
-        <Route path="/reports-masters" element={<ReportsMasters />} />
+        <Route path="/reportsmasters" element={<ReportsMasters />} />
         <Route path="/masters" element={<Master />} />
         <Route path="/patient" element={<Patient />} />
-        <Route path="/healthcare-practitioner" element={<HealthcarePractitioner />} />
+        <Route path="/HealthcarePractitioner" element={<HealthcarePractitioner />} />
         <Route path="/newpractitioner-schedule" element={<NewPractitionerSchedule />} />
         <Route path="/editPractitioner-schedule" element={<EditPractitionerSchedule />} />
         <Route path="/practitioner-schedule" element={<Practitioner />} />
-        <Route path="/medical-department" element={<MedicalDapartment />} />
-
-
-
-
-
-
-        <Route path="/consultation-setup" element={<ConsultationSetup />} />
-        <Route path="/appointment-type" element={<AppointmentType />} />
-        <Route path="/new-appointment-type" element={<NewAppointmentType />} />
-        <Route path="/edit-appointment-type/:id" element={<EditAppointmentType />} />
-
-
-        <Route path="/consultation" element={<Consultation />} />
-        <Route path="/patient-appointment" element={<PatientApointment />} />
-
-
-
-        <Route path="/facility-management" element={<FacilityManagement />} />
-        <Route path="/healthcare-service-unit-type" element={<HealthCareServiceUnit />} />
-      
-
 
 
       </Routes>
